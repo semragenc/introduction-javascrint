1d4a58b61edc5b465e0d1702d721781d8e7ec9bc
Semra Genc <semrademir292@gmail.com>
2024-07-09 02:15:43 +0300

    Arithmetic operators, loops and basic properties were tried

---
+++ js/index.js
@@ -0,0 +1,5 @@
+const name = "Semra"
+
+//console.log("Hello World"); //Sayfa yüklendiğinde consolda bu mesajı yazdır demek//
+//console.info("introduction to javascript") //aynı şeyi yapıyo console.log ile//
+//console.error("Hata oluştu")
